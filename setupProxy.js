0a1097edb8c5a4f70239e12bbc33b09e67eed361
Anita-Mutemi <anitajehu13@gmail.com>
2024-09-16 03:37:54 +0300

    add crm path

--- setupProxy.js
+++ setupProxy.js
@@ -4,7 +4,7 @@ export default function (app) {
   app.use(
     ['/', '/login', '/auth', '/v1', '/projects', '/me', '/v1/projects'],
     proxy({
-      target: 'https://crmv2.alphaterminal.pro',
+      target: 'https://young-refuge-51933-4129d1214e9f.herokuapp.com',
       changeOrigin: true,
     }),
   );
